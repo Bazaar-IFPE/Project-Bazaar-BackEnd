543b1dd5d3d71471efda7e90582406317c4df63c
danilohumberto” <danilohumberto12@hotmail.com>
2024-04-16 21:32:45 -0300

    starting project api

---
+++ src/test/java/com/example/bazaarapi/BazaarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bazaarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
