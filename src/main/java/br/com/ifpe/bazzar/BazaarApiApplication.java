030d8e5a5e829cd73450dd1db06a25879d1204e8
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-04-18 13:58:58 -0300

    Creating the initial project structure

--- src/main/java/br/com/ifpe/bazzar/BazaarApiApplication.java
+++ src/main/java/br/com/ifpe/bazzar/BazaarApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.bazaarapi;
+package br.com.ifpe.bazzar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
