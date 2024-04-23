eb102c4f775578f535bc993a249b5b8b26544513
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-04-23 11:15:59 -0300

    Added email sending functionality when new user is registered

--- src/main/java/br/com/ifpe/bazzar/modelo/usuario/UsuarioService.java
+++ src/main/java/br/com/ifpe/bazzar/modelo/usuario/UsuarioService.java
@@ -2,6 +2,7 @@ package br.com.ifpe.bazzar.modelo.usuario;
 
 import jakarta.transaction.Transactional;
 
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,20 @@ public class UsuarioService {
    @Autowired
    private UsuarioRepository repository;
 
+   @Autowired
+   private EmailService emailService;
+
    @Transactional
    public Usuario save(Usuario usuario) {
 
        usuario.setHabilitado(Boolean.TRUE);
        usuario.setVersao(1L);
-       //usuario.setDataCriacao(LocalDate.now());
+       usuario.setDataCriacao(LocalDate.now());
+    
+       //envio de email
+       emailService.enviarEmailTexto(usuario.getEmail(),"Novo usuário cadastrado","Você está recebendo um email de cadastro");
+
+       //salvando novo usuario
        return repository.save(usuario);
    }
 
