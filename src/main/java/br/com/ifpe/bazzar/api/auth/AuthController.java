9e872287cedf6a6488f268e6097a92b4e7b2c5cf
Geovane Júnior <geovanejosedasilvajr@gmail.com>
2024-05-30 21:15:48 -0300

    error send email password reset

--- src/main/java/br/com/ifpe/bazzar/api/auth/AuthController.java
+++ src/main/java/br/com/ifpe/bazzar/api/auth/AuthController.java
@@ -1,20 +1,29 @@
 package br.com.ifpe.bazzar.api.auth;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.bazzar.api.Dto.AuthenticationRequest;
 import br.com.ifpe.bazzar.api.Dto.PasswordResetRequest;
+import br.com.ifpe.bazzar.modelo.email.Emails;
+import br.com.ifpe.bazzar.modelo.email.EmailsRepository;
 import br.com.ifpe.bazzar.modelo.security.jwt.AuthService;
 import br.com.ifpe.bazzar.modelo.usuario.Usuario;
+import br.com.ifpe.bazzar.modelo.usuario.UsuarioRepository;
 import br.com.ifpe.bazzar.modelo.usuario.UsuarioService;
 
 @RestController
@@ -27,6 +36,15 @@ public class AuthController {
 
     @Autowired
     private UsuarioService usuarioService;
+
+    @Autowired
+    private EmailsRepository emailsRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostMapping(value = "/login")
     public ResponseEntity<?> login(@RequestBody AuthenticationRequest authDto) {
@@ -55,6 +73,37 @@ public class AuthController {
     } else {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado.");
     }
-}
+    }
+
+    @GetMapping("/password-reset")
+    public String showPasswordResetForm(@RequestParam("token") String token, Model model) {
+        model.addAttribute("token", token);
+        return "password-reset";
+    }
+
+    @PostMapping("/password-reset")
+    public String handlePasswordReset(@RequestParam("token") String token,
+                                      @RequestParam("newPassword") String newPassword,
+                                      @RequestParam("confirmPassword") String confirmPassword) {
+
+        if (!newPassword.equals(confirmPassword)) {
+            return "redirect:/password-reset?error=mismatch&token=" + token;
+        }
+
+        Emails emailPassword = emailsRepository.findByUuid(UUID.fromString(token));
+        if (emailPassword != null) {
+            if (emailPassword.getExpirationDate().compareTo(Instant.now()) >= 0) {
+                emailPassword.getUsuario().setSenha(passwordEncoder.encode(newPassword));
+                usuarioRepository.save(emailPassword.getUsuario());
+                emailsRepository.delete(emailPassword); // Token usado, removendo
+                return "redirect:/login?resetSuccess";
+            } else {
+                return "redirect:/password-reset?error=expired&token=" + token;
+            }
+        } else {
+            return "redirect:/password-reset?error=invalid&token=" + token;
+        }
+    }
+
 
 }
